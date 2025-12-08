f52b81fe0afbd2c82b2e542b25e58b529271331c
Liyacommits <liyabonamendela3@gmail.com>
2025-12-08 21:47:51 +0200

    I am tired

--- backend/services/llama_engine.py
+++ backend/services/llama_engine.py
@@ -1,6 +1,6 @@
 from groq import Groq
 
-client = Groq(api_key="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")
+client = Groq(api_key="groq ap key")
 
 def generate_response(prompt: str) -> str:
     response = client.chat.completions.create(
